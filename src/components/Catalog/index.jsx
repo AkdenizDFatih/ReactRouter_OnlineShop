ee8679cf77f7a316f6f2968669c451787d7aa518
gdiguglielmo <gusbertus@gmail.com>
2023-05-19 17:22:53 +0200

    initial push for refactored solution

---
+++ src/components/Catalog/index.jsx
@@ -0,0 +1,24 @@
+import './styles.scss';
+import { useSelector } from 'react-redux';
+import Item from '../Item/index.jsx';
+
+function Catalog() {
+  const items = useSelector((state) => state.items);
+
+  return (
+    <>
+      <div className='shop-wrapper'>
+        {items.map((item) => {
+          return (
+            <Item
+              key={item.id}
+              item={item}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Catalog;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,32 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Catalog from '../components/Catalog/index.jsx'
+import LoginPage from '../components/Authentication/LoginPage.jsx'
+import Cart from '../components/Cart/index.jsx'
+import Layout from './Layout.jsx'
+import ProtectedRoutesValidator from './ProtectedRoutesValidator.jsx'
+import RegisterPage from '../components/Authentication/RegisterPage.jsx'
+import ValidationPage from '../components/Authentication/ValidationPage.jsx'
+import NotFound from './NotFound.jsx'
+
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Catalog/>}/>
+                    <Route path="login" element={<LoginPage/>}/>
+                    <Route path="register">
+                        <Route index element={<RegisterPage/>}/>
+                        <Route path="validation" element={<ValidationPage/>}/>
+                    </Route>
+                    <Route path="" element={<ProtectedRoutesValidator/>}>
+                        <Route path="cart" element={<Cart/>}/>
+                    </Route>
+                    <Route path="*" element={<NotFound/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Router
